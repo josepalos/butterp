196b6a18cdcf5b09833d6f2d19036ef855799b6e
Josep <jap8@alumnes.udl.cat>
2016-03-06 20:11:54 +0100

    Lambda extends the environment for each call to apply, so each apply is independent. Binding the args doesn't evaluate them.

--- src/main/java/cat/udl/eps/butterp/data/Lambda.java
+++ src/main/java/cat/udl/eps/butterp/data/Lambda.java
@@ -4,18 +4,19 @@ import cat.udl.eps.butterp.environment.Environment;
 
 public class Lambda extends Function {
 
-    private final Environment local_environment;
+    private final Environment definition_env;
     private final SExpression params;
     private final SExpression body;
 
     public Lambda(SExpression params, SExpression body, Environment definitionEnv) {
-	local_environment = definitionEnv;
+	definition_env = definitionEnv;
 	this.params = params;
 	this.body = body;
     }
 
     @Override
     public SExpression apply(SExpression evargs, Environment callingEnv) {
+        Environment local_environment = definition_env.extend();
 	bindAll(local_environment, params, evargs);
 	return body.eval(local_environment);
     }
@@ -24,7 +25,7 @@ public class Lambda extends Function {
 	if (!symbols_list.equals(Symbol.NIL)) {
 	    e.bind(
 		    (Symbol) ListOps.car(symbols_list),
-		    ListOps.car(values).eval(e)
+		    ListOps.car(values)
 	    );
 	    bindAll(e, ListOps.cdr(symbols_list), ListOps.cdr(values));
 	}
